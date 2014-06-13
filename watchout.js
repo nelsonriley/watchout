5835282d2f23d96c04eaed6ad64d6915ce065ce3
nelsonriley <nelsonriley@gmail.com>
2014-06-13 15:57:10 -0700

    Drawing enemies on the game board

--- watchout.js
+++ watchout.js
@@ -1,2 +1,46 @@
 // start slingin' some d3 here.
 
+var width = 500,
+    height = 500,
+    radius = 10;
+
+var randomLocation = function() {
+  var spot = {};
+  spot.x = Math.random()*(width-(2*radius))+radius;
+  spot.y = Math.random()*(height-(2*radius))+radius;
+  return spot;
+};
+
+var svg = d3.select(".gameboard").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .append("g");
+
+var buildEnemies = function(n) {
+
+  var enemies = [];
+  for (var i = 0 ; i < n ; i++) {
+    enemies.push(i);
+  }
+  return enemies;
+
+};
+
+var updateEnemies = function(data) {
+  var enemies = svg.selectAll("circle")
+    .data(data);
+
+
+  enemies.enter().append("circle")
+    .attr("r", 10)
+    .attr("class", "enemy")
+    .attr("cy", function(d, i) {
+      return randomLocation().y
+    })
+    .attr("cx", function(d, i) {
+      return randomLocation().x
+    });
+
+};
+
+updateEnemies(buildEnemies(30));
